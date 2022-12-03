0bd689d1e2bc9ba53b4c9efb4403b9b8d7b56cf0
lojithv <lojithvinsuka1998@gmail.com>
2022-12-03 11:00:01 +0530

    changes

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -5,33 +5,24 @@ import {
   View,
 } from "react-native";
 
-function AboutScreen({ navigation }) {
+function AboutScreen() {
   return (
     <View style={{ padding: 50, height: 200, flex: 1, flexDirection: "column", justifyContent: 'space-around' }}>
       <View>
         <Text
           style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Student ID : 101243840
-        </Text>
-        <Text
-          style={{
-            //   height: 40,
             color: 'black',
             marginTop: 10,
           }}>
-          Full Name : Wissam Hamed
+          Name : Mohsen Yahya
+        </Text>
+        <Text
+          style={{
+            color: 'black',
+          }}>
+          ID : 101241827
         </Text>
       </View>
-
-      <Button
-        onPress={() => {
-          navigation.navigate("Main");
-        }}
-        title={"Go to Main"}
-      />
     </View>
   );
 }
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -8,26 +8,22 @@ import {
 } from 'react-native';
 
 function MainScreen({ navigation }) {
-  const [size, setSize] = useState("0");
+  const [roomSize, setRoomSize] = useState("0");
   const [flooringPricePerUnit, setFlooringPricePerUnit] = useState("0");
   const [installationCostPerUnit, setInstallationCostPerUnit] = useState("0");
 
-  const [installationCost, setInstallationCost] = useState(0);
-  const [flooringCost, setFlooringCost] = useState(0);
+  const [totalInstallationCost, setTotalInstallationCost] = useState(0);
+  const [totalFlooringCost, setTotalFlooringCost] = useState(0);
   const [tax, setTax] = useState(0);
 
-  useEffect(() => {
-    handleCalculation();
-  }, [size, flooringPricePerUnit, installationCostPerUnit]);
-
-  const handleCalculation = () => {
-    if (parseFloat(size)) {
+  const calculateEstimate = () => {
+    if (parseFloat(roomSize)) {
       const updatedInstallationCost =
-        parseFloat(size) * parseFloat(installationCostPerUnit);
-      setInstallationCost(updatedInstallationCost);
+        parseFloat(roomSize) * parseFloat(installationCostPerUnit);
+      setTotalInstallationCost(updatedInstallationCost);
 
-      const updatedFlooringCost = parseFloat(size) * parseFloat(flooringPricePerUnit);
-      setFlooringCost(updatedFlooringCost);
+      const updatedFlooringCost = parseFloat(roomSize) * parseFloat(flooringPricePerUnit);
+      setTotalFlooringCost(updatedFlooringCost);
 
       const updatedTax = (updatedFlooringCost + updatedInstallationCost) * 0.13;
       setTax(updatedTax);
@@ -44,107 +40,118 @@ function MainScreen({ navigation }) {
         justifyContent: 'space-between',
       }}>
       <View>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Size of a room
-        </Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            color: "black",
-            marginTop: 10,
+        <View>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Size of a room
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              color: "black",
+              marginTop: 10,
+            }}
+            placeholder="0"
+            onChangeText={newSize => setRoomSize(newSize)}
+            defaultValue={roomSize}
+          />
+        </View>
+        <View>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Price per unit of flooring
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              color: "black",
+              marginTop: 10,
+            }}
+            placeholder="0"
+            onChangeText={newCost => setFlooringPricePerUnit(newCost)}
+            defaultValue={flooringPricePerUnit}
+          />
+        </View>
+        <View>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Price per unit of flooring installation
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              color: "black",
+              marginTop: 10,
+            }}
+            placeholder="Type here to translate!"
+            onChangeText={newCost => setInstallationCostPerUnit(newCost)}
+            defaultValue={installationCostPerUnit}
+          />
+        </View>
+
+        <View style={{ marginTop: 10, marginBottom: 10 }}>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Installation cost before tax:{totalInstallationCost}
+          </Text>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Flooring cost before tax:{totalFlooringCost}
+          </Text>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Total cost (installation + flooring) before tax:
+            {(parseFloat(totalInstallationCost) + parseFloat(totalFlooringCost)).toString()}
+          </Text>
+          <Text
+            style={{
+              //   height: 40,
+              color: 'black',
+            }}>
+            Tax:{tax}
+          </Text>
+        </View>
+        <View style={{ marginTop: 10, marginBottom: 10 }}>
+        <Button
+          onPress={() => {
+            calculateEstimate();
           }}
-          placeholder="0"
-          onChangeText={newSize => setSize(newSize)}
-          defaultValue={size}
+          title={'Calculate'}
         />
       </View>
-      <View>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Price per unit of flooring
-        </Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            color: "black",
-            marginTop: 10,
-          }}
-          placeholder="0"
-          onChangeText={newCost => setFlooringPricePerUnit(newCost)}
-          defaultValue={flooringPricePerUnit}
-        />
-      </View>
-      <View>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Price per unit of flooring installation
-        </Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            color: "black",
-            marginTop: 10,
-          }}
-          placeholder="Type here to translate!"
-          onChangeText={newCost => setInstallationCostPerUnit(newCost)}
-          defaultValue={installationCostPerUnit}
-        />
       </View>
 
-      <View style={{ marginTop: 10, marginBottom: 10 }}>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Installation cost:{installationCost}
-        </Text>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Flooring cost:{flooringCost}
-        </Text>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Total cost (installation + flooring):
-          {(parseFloat(installationCost) + parseFloat(flooringCost)).toString()}
-        </Text>
-        <Text
-          style={{
-            //   height: 40,
-            color: 'black',
-          }}>
-          Tax:{tax}
-        </Text>
-      </View>
 
       <View style={{ marginTop: 10, marginBottom: 10 }}>
         <Button
           onPress={() => {
             navigation.navigate("About");
           }}
-          title={'Go to About'}
+          title={'About Me'}
         />
       </View>
     </ScrollView>
