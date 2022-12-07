986ddbf46f06fc328751ae56518d5fcee10b999e
lojithv <lojithvinsuka1998@gmail.com>
2022-12-07 21:40:53 +0530

    changes

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,183 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 import {
+  SafeAreaView,
   ScrollView,
+  StatusBar,
+  StyleSheet,
   TextInput,
-  Switch,
   Text,
   Button,
+  useColorScheme,
   View,
 } from 'react-native';
+import Section from '../components/Section';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-function MainScreen({ navigation }) {
-  const [roomSize, setRoomSize] = useState("0");
-  const [flooringPricePerUnit, setFlooringPricePerUnit] = useState("0");
-  const [installationCostPerUnit, setInstallationCostPerUnit] = useState("0");
-
-  const [totalInstallationCost, setTotalInstallationCost] = useState(0);
-  const [totalFlooringCost, setTotalFlooringCost] = useState(0);
-  const [tax, setTax] = useState(0);
-
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
-  const calculateEstimate = () => {
-    if (parseFloat(roomSize)) {
-      const updatedInstallationCost =
-        parseFloat(roomSize) * parseFloat(installationCostPerUnit);
-      setTotalInstallationCost(updatedInstallationCost);
-
-      const updatedFlooringCost = parseFloat(roomSize) * parseFloat(flooringPricePerUnit);
-      setTotalFlooringCost(updatedFlooringCost);
-
-      const updatedTax = (updatedFlooringCost + updatedInstallationCost) * 0.13;
-      setTax(updatedTax);
-    }
-  };
+function MainScreen({navigation}) {
+  const [isHungry, setIsHungry] = useState(true);
 
   return (
-    <ScrollView
-      contentContainerStyle={{
-        padding: 50,
-        height: 200,
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}>
-      <View>
-        <View>
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Text
-              style={{
-                //   height: 40,
-                color: 'black',
-              }}>
-              Size of a room
-            </Text>
-            <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={'white'}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitch}
-              value={isEnabled}
-            />
-          </View>
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: 'gray',
-                borderWidth: 1,
-                color: "black",
-                marginTop: 10,
-                width: '70%'
-              }}
-              placeholder="0"
-              onChangeText={newSize => setRoomSize(newSize)}
-              defaultValue={roomSize}
-            />
-            <Text
-              style={{
-                color: 'black',
-              }}>
-              {isEnabled ? 'sq ft' : 'sq mt'}
-            </Text>
-          </View>
-
-        </View>
-        <View>
-          <Text
-            style={{
-              //   height: 40,
-              color: 'black',
-            }}>
-            Price per unit of flooring
-          </Text>
-          <TextInput
-            style={{
-              height: 40,
-              borderColor: 'gray',
-              borderWidth: 1,
-              color: "black",
-              marginTop: 10,
-            }}
-            placeholder="0"
-            onChangeText={newCost => setFlooringPricePerUnit(newCost)}
-            defaultValue={flooringPricePerUnit}
-          />
-        </View>
-        <View>
-          <Text
-            style={{
-              //   height: 40,
-              color: 'black',
-            }}>
-            Price per unit of flooring installation
-          </Text>
-          <TextInput
-            style={{
-              height: 40,
-              borderColor: 'gray',
-              borderWidth: 1,
-              color: "black",
-              marginTop: 10,
-            }}
-            placeholder="0"
-            onChangeText={newCost => setInstallationCostPerUnit(newCost)}
-            defaultValue={installationCostPerUnit}
-          />
-        </View>
-
-        <View style={{ marginTop: 10, marginBottom: 10 }}>
-          <Text
-            style={{
-              //   height: 40,
-              color: 'black',
-            }}>
-            Installation cost before tax:{totalInstallationCost}
-          </Text>
-          <Text
-            style={{
-              //   height: 40,
-              color: 'black',
-            }}>
-            Flooring cost before tax:{totalFlooringCost}
-          </Text>
-          <Text
-            style={{
-              //   height: 40,
-              color: 'black',
-            }}>
-            Total cost (installation + flooring) before tax:
-            {(parseFloat(totalInstallationCost) + parseFloat(totalFlooringCost)).toString()}
-          </Text>
-          <Text
-            style={{
-              //   height: 40,
-              color: 'black',
-            }}>
-            Tax:{tax}
-          </Text>
-        </View>
-        <View style={{ marginTop: 10, marginBottom: 10 }}>
-          <Button
-            onPress={() => {
-              calculateEstimate();
-            }}
-            title={'Calculate'}
-          />
-        </View>
-      </View>
-
-
-      <View style={{ marginTop: 10, marginBottom: 10 }}>
-        <Button
-          onPress={() => {
-            navigation.navigate("About");
-          }}
-          title={'About Me'}
-        />
-      </View>
-    </ScrollView>
+    <View>
+      <Text
+        style={{
+          //   height: 40,
+          color: 'black',
+        }}>
+        Main screen
+      </Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          color: "black",,
+        }}
+        defaultValue="Name me!"
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate("Details");;
+        }}
+        title={'Go to About'}
+      />
+    </View>
   );
 }
 
